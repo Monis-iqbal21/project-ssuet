22fd5e34ecd4dc1f756114659b70e623f98866b2
Monis_iqbal <150613063+Monis-iqbal21@users.noreply.github.com>
2025-01-18 21:12:03 +0500

    Update login style

--- client/src/app/pages/login/page.jsx
+++ client/src/app/pages/login/page.jsx
@@ -1,88 +1,3 @@
-// import React from "react";
-// import Image from "next/image";
-// import "./login.css";
-// import nikeLogo from "../../../../public/images/logo2.png";
-
-// const page = () => {
-
-
-
-  
-//   return (
-//     <div className="login_container">
-//       <div className="logo_heading_div">
-//         <Image src={nikeLogo} alt="nike logo"></Image>
-//         <h2>YOUR ACCOUNT FOR EVERYTHING NIKE</h2>
-//       </div>
-
-//       <div className="login_input_div">
-//         <input
-//           type="email"
-//           placeholder="Email address"
-//           className="login_input"
-//         />
-//         <input type="password" placeholder="Password" className="login_input" />
-//       </div>
-
-//       <div className="login_checkbox_forgot_div">
-//         <p
-//           style={{
-//             fontSize: "12px",
-//             fontWeight: 400,
-//             display: "flex",
-//             columnGap: "10px",
-//             color: "#8D8D8D",
-//           }}
-//         >
-//           <input type="checkbox" name="" id="" style={{width:"20px" ,accentColor:"white"}} />
-//           Keep me signed in
-//         </p>
-//         <button style={{ fontSize: "12px", fontWeight: 400, color: "#BCBCBC",  }}>
-//           Forgot your password?
-//         </button>
-//       </div>
-//       <p
-//         style={{
-//           fontSize: "12px",
-//           fontWeight: 400,
-//           color: "#8D8D8D",
-//           width: "80%",
-//           margin: "0 auto",
-//           textAlign: "center",
-//         }}
-//       >
-//         By logging in, you agree to Nike's{" "}
-//         <span style={{ textDecoration: "underline" }}> Privacy Policy</span> and{" "}
-//         <span style={{ textDecoration: "underline" }}> Terms of Use. </span>
-//       </p>
-//       <button className="login_btn">SIGN IN</button>
-//       <p className="login_join_us">
-//         Not a Memeber? <span>Join US</span>
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default page;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 "use client";
 
 import React, { useState } from "react";
@@ -150,8 +65,8 @@ const LoginPage = () => {
       </form>
 
       <div className="login_checkbox_forgot_div">
-        <p style={{ fontSize: "12px", color: "#8D8D8D" }}>
-          <input type="checkbox" style={{ width: "20px", accentColor: "white" }} />
+        <p style={{ fontSize: "12px", color: "#8D8D8D",display:"flex",alignItems:"center" }}>
+          <input type="checkbox" style={{ width: "18px", height: "18px", accentColor: "white" }} />
           Keep me signed in
         </p>
         <button style={{ fontSize: "12px", color: "#BCBCBC" }}>
@@ -159,12 +74,22 @@ const LoginPage = () => {
         </button>
       </div>
       <p style={{ fontSize: "12px", color: "#8D8D8D", textAlign: "center" }}>
-        By logging in, you agree to Nike's{" "}
-        <span style={{ textDecoration: "underline" }}>Privacy Policy</span> and{" "}
+        By logging in, you agree to Nike's&nbsp;
+        <span style={{ textDecoration: "underline" }}>Privacy Policy</span> &nbsp;and&nbsp;
         <span style={{ textDecoration: "underline" }}>Terms of Use</span>.
       </p>
-      <p className="login_join_us">
-        Not a Member? <span>Join Us</span>
+      <p className="login_join_us" style={{fontSize:"15px",fontWeight:"500"}}>
+        Not a Member? &nbsp;
+        <Link
+          href={"/pages/register"}
+          style={{
+            color: "black",
+            fontWeight: "500",
+            textDecoration: "underline",
+          }}
+        >
+          Join Us
+        </Link>
       </p>
     </div>
   );
